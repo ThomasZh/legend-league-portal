5f6e2c0d4facacd5369fe2c3f90a6719e4b79c4f
ThomasZhang <thomas.zh@qq.com>
2017-01-24 06:45:07 +0800

    init project

---
+++ router.py
@@ -0,0 +1,44 @@
+# _*_ coding: utf-8_*_
+#
+# genral application route config:
+# simplify the router config by dinamic load class
+# by lwz7512
+# @2016/05/17
+
+import tornado.web
+
+from foo import comm
+from foo.ui import ui_cn
+
+
+def map():
+
+    config = [
+
+        # GET: 根据 HTTP header 收集客户端相关信息：是否手机、操作系统、浏览器等信息。
+        (r'/', getattr(ui_cn, 'UicnIndexHandler')),
+
+        (r'/uicn', getattr(ui_cn, 'UicnIndexHandler')),
+        (r'/uicn/list', getattr(ui_cn, 'UicnListHandler')),
+        (r'/uicn/exp', getattr(ui_cn, 'UicnExpHandler')),
+        (r'/uicn/book', getattr(ui_cn, 'UicnBookHandler')),
+        (r'/uicn/study', getattr(ui_cn, 'UicnStudyHandler')),
+        (r'/uicn/game', getattr(ui_cn, 'UicnGameHandler')),
+        (r'/uicn/peixun', getattr(ui_cn, 'UicnPeixunHandler')),
+        (r'/uicn/topic', getattr(ui_cn, 'UicnTopicHandler')),
+        (r'/uicn/online', getattr(ui_cn, 'UicnOnlineHandler')),
+        (r'/uicn/zhaopin', getattr(ui_cn, 'UicnZhaopinHandler')),
+        # 注册 登录 找回
+        (r'/auth/login', getattr(ui_cn, 'AuthLoginHandler')),
+        (r'/auth/getpass', getattr(ui_cn, 'AuthGetpassHandler')),
+        (r'/auth/reg', getattr(ui_cn, 'AuthRegHandler')),
+        (r'/auth/findpassbymail', getattr(ui_cn, 'AuthFindpassHandler')),
+        (r'/auth/changepass', getattr(ui_cn, 'AuthChangepassHandler')),
+        (r'/auth/editsuccess', getattr(ui_cn, 'AuthEditsuccessHandler')),
+
+        # comm
+        ('.*', getattr(comm, 'PageNotFoundHandler'))
+
+    ]
+
+    return config
